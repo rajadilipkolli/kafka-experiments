dba53e308965a1126280dbd1afd958bd25c652ea
Raja Dilip Chowdary Kolli <raja.kolli@exxonmobil.com>
2020-01-06 07:32:55 +0530

    upgrade to latest version

--- src/main/java/com/example/analytics/processor/PageViewEventProcessor.java
+++ src/main/java/com/example/analytics/processor/PageViewEventProcessor.java
@@ -1,18 +1,19 @@
 package com.example.analytics.processor;
 
-import com.example.analytics.binding.AnalyticsBinding;
-import com.example.analytics.model.PageViewEvent;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Serialized;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
+
+import com.example.analytics.binding.AnalyticsBinding;
+import com.example.analytics.model.PageViewEvent;
 
 @Component
 public class PageViewEventProcessor {
@@ -24,7 +25,7 @@ public class PageViewEventProcessor {
     return eventKStream
         .filter((key, value) -> value.getDuration() > 10)
         .map((key, value) -> new KeyValue<>(value.getPage(), "0"))
-        .groupByKey(Serialized.with(Serdes.String(), Serdes.String()))
+        .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
 //        .windowedBy(TimeWindows.of(1000 * 60))
         .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as(AnalyticsBinding.PAGE_COUNT_MV)
             .withKeySerde(Serdes.String())
--- src/test/java/com/example/analytics/AnalyticsApplicationTests.java
+++ src/test/java/com/example/analytics/AnalyticsApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.analytics;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude="
+    + "org.springframework.cloud.stream.test.binder.TestSupportBinderAutoConfiguration")
 public class AnalyticsApplicationTests {
 
   @Test
