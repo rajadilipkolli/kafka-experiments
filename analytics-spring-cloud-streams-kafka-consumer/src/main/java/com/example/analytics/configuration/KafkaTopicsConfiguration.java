c30f5c62d86c34eaf66077005bbd3721c3402871
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-10-14 08:13:42 +0530

    Using Builder pattern to build topics

--- analytics-spring-cloud-streams-kafka-consumer/src/main/java/com/example/analytics/configuration/KafkaTopicsConfiguration.java
+++ analytics-spring-cloud-streams-kafka-consumer/src/main/java/com/example/analytics/configuration/KafkaTopicsConfiguration.java
@@ -1,49 +1,51 @@
 package com.example.analytics.configuration;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicsConfiguration {
 
-    private final AnalyticsApplicationProperties analyticsApplicationProperties;
+  private final AnalyticsApplicationProperties analyticsApplicationProperties;
 
-    public KafkaTopicsConfiguration(AnalyticsApplicationProperties analyticsApplicationProperties) {
-        this.analyticsApplicationProperties = analyticsApplicationProperties;
-    }
+  public KafkaTopicsConfiguration(AnalyticsApplicationProperties analyticsApplicationProperties) {
+    this.analyticsApplicationProperties = analyticsApplicationProperties;
+  }
 
-    @Bean
-    public NewTopic kafkaStreamspcmvRePartitionTopic() {
-        return new NewTopic(
-                analyticsApplicationProperties.topicNameRePartition(),
-                analyticsApplicationProperties.partitions(),
-                analyticsApplicationProperties.replication());
-    }
+  @Bean
+  public NewTopic kafkaStreamspcmvRePartitionTopic() {
+    return TopicBuilder.name(analyticsApplicationProperties.topicNameRePartition())
+        .partitions(analyticsApplicationProperties.partitions())
+        .replicas(analyticsApplicationProperties.replication())
+        .build();
+  }
 
-    @Bean
-    public NewTopic kafkaStreamspcmvChangeLogTopic() {
-        return new NewTopic(
-                analyticsApplicationProperties.topicNameChangelog(),
-                analyticsApplicationProperties.partitions(),
-                analyticsApplicationProperties.replication());
-    }
+  @Bean
+  public NewTopic kafkaStreamspcmvChangeLogTopic() {
+    return TopicBuilder.name(analyticsApplicationProperties.topicNameChangelog())
+        .partitions(analyticsApplicationProperties.partitions())
+        .replicas(analyticsApplicationProperties.replication())
+        .build();
+  }
 
-    @Bean
-    public NewTopic pcsTopic() {
+  @Bean
+  public NewTopic pcsTopic() {
+    return TopicBuilder.name(analyticsApplicationProperties.topicNamePcs())
+        .partitions(analyticsApplicationProperties.partitions())
+        .replicas(analyticsApplicationProperties.replication())
+        .config(TopicConfig.COMPRESSION_TYPE_CONFIG, "zstd")
+        .build();
+  }
 
-        return new NewTopic(
-                analyticsApplicationProperties.topicNamePcs(),
-                analyticsApplicationProperties.partitions(),
-                analyticsApplicationProperties.replication());
-    }
-
-    @Bean
-    public NewTopic pvsTopic() {
-
-        return new NewTopic(
-                analyticsApplicationProperties.topicNamePvs(),
-                analyticsApplicationProperties.partitions(),
-                analyticsApplicationProperties.replication());
-    }
+  @Bean
+  public NewTopic pvsTopic() {
+    return TopicBuilder.name(analyticsApplicationProperties.topicNamePvs())
+        .partitions(analyticsApplicationProperties.partitions())
+        .replicas(analyticsApplicationProperties.replication())
+        .config(TopicConfig.COMPRESSION_TYPE_CONFIG, "zstd")
+        .build();
+  }
 }
