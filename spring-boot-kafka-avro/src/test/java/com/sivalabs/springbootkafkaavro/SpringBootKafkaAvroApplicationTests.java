a7018d996b70139e0820d32e440a106be02021ce
rajadileepkolli <rajadileepkolli@gmail.com>
2021-12-26 14:18:54 +0530

    fixes issue with avro project

--- spring-boot-kafka-avro/src/test/java/com/sivalabs/springbootkafkaavro/SpringBootKafkaAvroApplicationTests.java
+++ spring-boot-kafka-avro/src/test/java/com/sivalabs/springbootkafkaavro/SpringBootKafkaAvroApplicationTests.java
@@ -1,5 +1,6 @@
 package com.sivalabs.springbootkafkaavro;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -15,6 +16,7 @@ import java.time.Duration;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.testcontainers.containers.KafkaContainer.KAFKA_PORT;
 
 @SpringBootTest
 @Testcontainers
@@ -23,45 +25,60 @@ public class SpringBootKafkaAvroApplicationTests {
   private static final DockerImageName KAFKA_TEST_IMAGE =
       DockerImageName.parse("confluentinc/cp-kafka:6.1.0");
 
-  private static final DockerImageName KAFKA_SCHEMA_IMAGE =
-      DockerImageName.parse("confluentinc/cp-schema-registry:6.1.0");
-
   private static final Duration startupTimeout = Duration.ofMinutes(5);
 
   @Container
   public static final KafkaContainer KAFKA =
       new KafkaContainer(KAFKA_TEST_IMAGE).withStartupTimeout(startupTimeout);
 
-  @Container
-  public static final GenericContainer KAFKA_SCHEMA =
-      new GenericContainer(KAFKA_SCHEMA_IMAGE)
-          .withExposedPorts(8081)
-          .withEnv("SCHEMA_REGISTRY_HOST_NAME", "0.0.0.0")
-          .withEnv(
-              "SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
-              "PLAINTEXT://0.0.0.0:9093,BROKER://0.0.0.0:9092")
-          .withStartupTimeout(startupTimeout);
+  public static class SchemaRegistryContainer extends GenericContainer<SchemaRegistryContainer> {
+    public SchemaRegistryContainer() {
+      this("6.1.0");
+    }
 
-  static {
+    public SchemaRegistryContainer(String version) {
+      super("confluentinc/cp-schema-registry:" + version);
+      withExposedPorts(8081);
+    }
+
+    public SchemaRegistryContainer withKafka(KafkaContainer kafka) {
+      withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry");
+      withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081");
+      withEnv(
+          "SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
+          "PLAINTEXT://" + kafka.getNetworkAliases().get(0) + ":9092");
+      return self();
+    }
+
+    public String getSchemaRegistryUrl() {
+      return "http://" + getContainerIpAddress() + ":" + getMappedPort(8081);
+    }
+  }
+
+  private static final SchemaRegistryContainer KAFKA_SCHEMA =
+      new SchemaRegistryContainer("6.1.0").withKafka(KAFKA).withStartupTimeout(startupTimeout);
+
+  @BeforeAll
+  static void startContainers() {
     Startables.deepStart(Stream.of(KAFKA, KAFKA_SCHEMA)).join();
   }
 
   @DynamicPropertySource
   static void registerPgProperties(DynamicPropertyRegistry registry) {
     registry.add(
-        "spring.kafka.consumer.bootstrap-servers", () -> KAFKA.getBootstrapServers().substring(12));
+        "spring.kafka.consumer.bootstrap-servers",
+        () -> String.format("http://%s:%s", KAFKA.getHost(), KAFKA.getMappedPort(KAFKA_PORT)));
     registry.add(
-        "spring.kafka.producer.bootstrap-servers", () -> KAFKA.getBootstrapServers().substring(12));
+        "spring.kafka.producer.bootstrap-servers",
+        () -> String.format("http://%s:%s", KAFKA.getHost(), KAFKA.getMappedPort(KAFKA_PORT)));
+    registry.add("spring.kafka.properties.schema.registry.url", KAFKA_SCHEMA::getSchemaRegistryUrl);
     registry.add(
-        "spring.kafka.properties.schema.registry.url",
-        () -> KAFKA_SCHEMA.getHost() + ":" + KAFKA_SCHEMA.getMappedPort(8081));
-    registry.add(
-        "spring.kafka.producer.properties.schema.registry.url",
-        () -> KAFKA_SCHEMA.getHost() + ":" + KAFKA_SCHEMA.getMappedPort(8081));
+        "spring.kafka.producer.properties.schema.registry.url", KAFKA_SCHEMA::getSchemaRegistryUrl);
   }
 
   @Test
   void contextLoads() {
     assertThat(KAFKA.isRunning()).isTrue();
+    assertThat(KAFKA_SCHEMA.isRunning()).isTrue();
   }
 }
