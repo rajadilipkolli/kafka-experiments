9f89e068c707c6e79cf6e5833a5346250f075351
Raja Kolli <rajadilipkolli@gmail.com>
2022-11-07 19:05:03 +0530

    Increase the kafka version

--- analytics-spring-cloud-streams-kafka-producer/src/test/java/com/example/analytics/AnalyticsProducerApplicationIntegrationTest.java
+++ analytics-spring-cloud-streams-kafka-producer/src/test/java/com/example/analytics/AnalyticsProducerApplicationIntegrationTest.java
@@ -39,7 +39,7 @@ import org.testcontainers.utility.DockerImageName;
 class AnalyticsProducerApplicationIntegrationTest {
 
   private static final DockerImageName KAFKA_TEST_IMAGE =
-      DockerImageName.parse("confluentinc/cp-kafka:5.3.6-1");
+      DockerImageName.parse("confluentinc/cp-kafka:5.3.8-1");
 
   @Container public static final KafkaContainer KAFKA = new KafkaContainer(KAFKA_TEST_IMAGE);
 
