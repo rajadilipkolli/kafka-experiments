f7e8ef877b540c3679348e0c7317dc1e54a5dda4
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-15 18:19:57 +0000

    fixes issue starting in local

--- kafka-reactor/boot-kafka-reactor-producer/src/main/java/com/example/boot/kafka/reactor/config/KafkaConfiguration.java
+++ kafka-reactor/boot-kafka-reactor-producer/src/main/java/com/example/boot/kafka/reactor/config/KafkaConfiguration.java
@@ -1,15 +1,24 @@
 package com.example.boot.kafka.reactor.config;
 
 import com.example.boot.kafka.reactor.dto.MessageDTO;
+import com.example.boot.kafka.reactor.util.AppConstants;
 import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 
+@EnableKafka
 @Configuration(proxyBeanMethods = false)
 public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic myTopic() {
+        return new NewTopic(AppConstants.HELLO_TOPIC, 1, (short) 1);
+    }
 
     @Bean
     public KafkaSender<Integer, MessageDTO> reactiveKafkaSender(KafkaProperties properties) {
