8bf1763a8b2db1d695ce0be8fc9b20387bfc0aff
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-15 20:07:56 +0000

    refactor junits

--- kafka-reactor/boot-kafka-reactor-producer/src/test/java/com/example/boot/kafka/reactor/BootKafkaReactorProducerApplicationTests.java
+++ kafka-reactor/boot-kafka-reactor-producer/src/test/java/com/example/boot/kafka/reactor/BootKafkaReactorProducerApplicationTests.java
@@ -1,11 +1,75 @@
 package com.example.boot.kafka.reactor;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = TestBootKafkaReactorProducerApplication.class)
+import com.example.boot.kafka.reactor.config.MyTestContainers;
+import com.example.boot.kafka.reactor.entity.MessageDTO;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOffset;
+import reactor.test.StepVerifier;
+
+@Slf4j
+@SpringBootTest(classes = {MyTestContainers.class})
+@AutoConfigureWebTestClient
 class BootKafkaReactorProducerApplicationTests {
 
+    @Autowired
+    KafkaReceiver<Integer, MessageDTO> receiver;
+
+    @Autowired
+    protected WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() throws InterruptedException {
+        assertThat(receiver).isNotNull();
+    }
+
+    @Test
+    void loadDataAndConsume() throws InterruptedException {
+        String requestBody =
+                """
+                {
+                    "id": 10000,
+                    "text": "hello1",
+                    "sentAt": "2023-06-15T18:49:38.813Z"
+                }
+                    """;
+        this.webTestClient
+                .post()
+                .uri("/messages")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(requestBody), String.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+
+        TimeUnit.SECONDS.sleep(5);
+        Flux<MessageDTO> flux = receiver.receive().map(record -> {
+            ReceiverOffset offset = record.receiverOffset();
+            var value = record.value();
+            log.debug(
+                    "Received message: topic-partition={} offset={} timestamp={} key={} value={}",
+                    offset.topicPartition(),
+                    offset.offset(),
+                    LocalDateTime.now(),
+                    record.key(),
+                    value);
+            offset.acknowledge();
+            return value;
+        });
+        assertThat(flux).isNotNull();
+
+        StepVerifier.create(flux).expectNextCount(1).verifyComplete();
+    }
 }
--- kafka-reactor/boot-kafka-reactor-producer/src/test/java/com/example/boot/kafka/reactor/TestBootKafkaReactorProducerApplication.java
+++ kafka-reactor/boot-kafka-reactor-producer/src/test/java/com/example/boot/kafka/reactor/TestBootKafkaReactorProducerApplication.java
@@ -1,47 +1,13 @@
 package com.example.boot.kafka.reactor;
 
-import static com.example.boot.kafka.reactor.util.AppConstants.HELLO_TOPIC;
-
-import com.example.boot.kafka.reactor.entity.MessageDTO;
-import java.util.Collections;
-import lombok.extern.slf4j.Slf4j;
+import com.example.boot.kafka.reactor.config.MyTestContainers;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
-import reactor.kafka.receiver.KafkaReceiver;
-import reactor.kafka.receiver.ReceiverOptions;
+import org.springframework.context.annotation.Import;
 
 @TestConfiguration(proxyBeanMethods = false)
-@Slf4j
+@Import(MyTestContainers.class)
 public class TestBootKafkaReactorProducerApplication {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.3-alpine"));
-    }
-
-    @Bean
-    @ServiceConnection
-    KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka").withTag("7.4.0")).withKraft();
-    }
-
-    @Bean
-    KafkaReceiver<Integer, MessageDTO> receiver(KafkaProperties properties) {
-        ReceiverOptions<Integer, MessageDTO> receiverOptions = ReceiverOptions.<Integer, MessageDTO>create(
-                        properties.buildConsumerProperties())
-                .subscription(Collections.singleton(HELLO_TOPIC))
-                .addAssignListener(partitions -> log.debug("onPartitionsAssigned {}", partitions))
-                .addRevokeListener(partitions -> log.debug("onPartitionsRevoked {}", partitions));
-
-        return KafkaReceiver.create(receiverOptions);
-    }
 
     public static void main(String[] args) {
         SpringApplication.from(BootKafkaReactorProducerApplication::main)
