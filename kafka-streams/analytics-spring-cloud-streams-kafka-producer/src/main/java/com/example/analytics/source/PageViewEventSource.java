03c1308eced8d8a583043bfc998f9ec7181eb3c5
Raja Kolli <rajadilipkolli@gmail.com>
2023-07-29 16:39:25 +0530

    Using reactive binder for publishing events

--- kafka-streams/analytics-spring-cloud-streams-kafka-producer/src/main/java/com/example/analytics/source/PageViewEventSource.java
+++ kafka-streams/analytics-spring-cloud-streams-kafka-producer/src/main/java/com/example/analytics/source/PageViewEventSource.java
@@ -1,15 +1,19 @@
-/* Licensed under Apache-2.0 2019-2022 */
+/* Licensed under Apache-2.0 2019-2023 */
 package com.example.analytics.source;
 
 import com.example.analytics.model.PageViewEvent;
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration(proxyBeanMethods = false)
 public class PageViewEventSource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PageViewEventSource.class);
 
     @Bean
     public Supplier<PageViewEvent> pageViewEventSupplier() {
@@ -18,7 +22,10 @@ public class PageViewEventSource {
         return () -> {
             String rPage = pages.get(new SecureRandom().nextInt(pages.size()));
             String rName = names.get(new SecureRandom().nextInt(names.size()));
-            return new PageViewEvent(rName, rPage, new SecureRandom().nextInt(10) > 5 ? 10 : 1000);
+            PageViewEvent pageViewEvent =
+                    new PageViewEvent(rName, rPage, new SecureRandom().nextInt(10) > 5 ? 10 : 1000);
+            LOGGER.info("Publishing Event :{}", pageViewEvent);
+            return pageViewEvent;
         };
     }
 }
