d8493b2742b9cd12f34f2f0bca1cc8613748a0b5
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-12 14:04:27 +0000

    fixes issue with documentation

--- kafka-sample/spring-boot/README.md
+++ kafka-sample/spring-boot/README.md
@@ -3,3 +3,7 @@
 This sample demonstrates sending message to topic (test_1), after listening to it will send the same message to (test_2).
 
 test_2 topic is configured for validation and if it fails then it will be retried for 3 times and moved to deadletter queue using non blocking way
+
+
+### URLS
+* KafkaUI : http://localhost:8080/springwolf/asyncapi-ui.html
--- kafka-sample/spring-boot/src/main/java/com/example/springbootkafkasample/service/sender/Sender.java
+++ kafka-sample/spring-boot/src/main/java/com/example/springbootkafkasample/service/sender/Sender.java
@@ -1,6 +1,11 @@
 package com.example.springbootkafkasample.service.sender;
 
+import static org.springframework.kafka.support.mapping.AbstractJavaTypeMapper.DEFAULT_CLASSID_FIELD_NAME;
+
 import com.example.springbootkafkasample.dto.MessageDTO;
+import io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata.annotation.AsyncOperation;
+import io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata.annotation.AsyncPublisher;
+import io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata.annotation.KafkaAsyncOperationBinding;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +23,21 @@ public class Sender {
         this.template = template;
     }
 
+    @AsyncPublisher(
+            operation =
+                    @AsyncOperation(
+                            channelName = "producer-topic",
+                            description = "MessageDTO payload",
+                            headers =
+                                    @AsyncOperation.Headers(
+                                            schemaName = "SpringKafkaDefaultHeaders",
+                                            values = {
+                                                @AsyncOperation.Headers.Header(
+                                                        name = DEFAULT_CLASSID_FIELD_NAME,
+                                                        description = "Spring Type Id Header",
+                                                        value = "com.example.springbootkafkasample.dto.MessageDTO"),
+                                            })))
+    @KafkaAsyncOperationBinding
     public void send(MessageDTO messageDTO) {
         this.template.send(messageDTO.topic(), UUID.randomUUID(), messageDTO).whenComplete((result, ex) -> {
             if (ex == null) {
