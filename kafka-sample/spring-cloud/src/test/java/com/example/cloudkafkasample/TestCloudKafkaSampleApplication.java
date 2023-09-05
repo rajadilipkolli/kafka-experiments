356728e7ae7ef79ab8ad3f1f613367d9338ebd20
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-05 07:26:36 +0000

    fix junits

--- kafka-sample/spring-cloud/src/test/java/com/example/cloudkafkasample/TestCloudKafkaSampleApplication.java
+++ kafka-sample/spring-cloud/src/test/java/com/example/cloudkafkasample/TestCloudKafkaSampleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,8 +14,12 @@ class TestCloudKafkaSampleApplication {
 
     @Bean
     @ServiceConnection
-    KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka").withTag("7.5.0")).withKraft();
+    KafkaContainer kafkaContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        KafkaContainer kafkaContainer = new KafkaContainer(
+                        DockerImageName.parse("confluentinc/cp-kafka").withTag("7.5.0"))
+                .withKraft();
+        dynamicPropertyRegistry.add("spring.cloud.stream.kafka.binder.brokers", kafkaContainer::getBootstrapServers);
+        return kafkaContainer;
     }
 
     public static void main(String[] args) {
