02e5454718510184407ca7fe6dbddfa8e09126e0
Raja Kolli <rajadilipkolli@gmail.com>
2023-10-24 05:01:26 +0000

    polish : remove unnecessary declaration

--- kafka-sample/spring-boot-multiple-producers-consumers/src/test/java/com/example/springbootkafka/multi/TestSpringBootKafkaMultiApplication.java
+++ kafka-sample/spring-boot-multiple-producers-consumers/src/test/java/com/example/springbootkafka/multi/TestSpringBootKafkaMultiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,12 +12,8 @@ public class TestSpringBootKafkaMultiApplication {
 
     @Bean
     @ServiceConnection
-    KafkaContainer kafkaContainer(DynamicPropertyRegistry propertyRegistry) {
-        KafkaContainer kafkaContainer = new KafkaContainer(
-                        DockerImageName.parse("confluentinc/cp-kafka").withTag("7.5.0"))
-                .withKraft();
-        propertyRegistry.add("spring.kafka.bootstrapServers", kafkaContainer::getBootstrapServers);
-        return kafkaContainer;
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka").withTag("7.5.1")).withKraft();
     }
 
     public static void main(String[] args) {
