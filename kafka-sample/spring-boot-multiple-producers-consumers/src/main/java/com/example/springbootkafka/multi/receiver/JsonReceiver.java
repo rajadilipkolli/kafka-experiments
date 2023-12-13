16c730e358e68b50b7b5e96d0704e15c7da938dc
Raja Kolli <rajadilipkolli@gmail.com>
2023-12-13 18:56:57 +0000

    polish: using lombok

--- kafka-sample/spring-boot-multiple-producers-consumers/src/main/java/com/example/springbootkafka/multi/receiver/JsonReceiver.java
+++ kafka-sample/spring-boot-multiple-producers-consumers/src/main/java/com/example/springbootkafka/multi/receiver/JsonReceiver.java
@@ -5,12 +5,14 @@ import static com.example.springbootkafka.multi.util.AppConstants.TOPIC_TEST_2;
 import com.example.springbootkafka.multi.domain.SimpleMessage;
 import jakarta.validation.Valid;
 import java.util.concurrent.CountDownLatch;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+@Getter
 @Component
 @Slf4j
 @Validated
@@ -18,13 +20,9 @@ public class JsonReceiver {
 
     private final CountDownLatch latch = new CountDownLatch(1);
 
-    public CountDownLatch getLatch() {
-        return latch;
-    }
-
     @KafkaListener(topics = TOPIC_TEST_2, containerFactory = "jsonKafkaListenerContainerFactory")
-    public void listen2(@Payload @Valid SimpleMessage cr) {
-        log.info(TOPIC_TEST_2 + " Received a message with key=" + cr.key() + ", value=" + cr.value());
+    public void jsonListener(@Payload @Valid SimpleMessage cr) {
+        log.info("{} Received a message with key = {} , value={}", TOPIC_TEST_2, cr.key(), cr.value());
         latch.countDown();
     }
 }
--- kafka-sample/spring-boot-multiple-producers-consumers/src/main/java/com/example/springbootkafka/multi/receiver/SimpleReceiver.java
+++ kafka-sample/spring-boot-multiple-producers-consumers/src/main/java/com/example/springbootkafka/multi/receiver/SimpleReceiver.java
@@ -3,26 +3,22 @@ package com.example.springbootkafka.multi.receiver;
 import static com.example.springbootkafka.multi.util.AppConstants.TOPIC_TEST_1;
 
 import java.util.concurrent.CountDownLatch;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
+@Slf4j
 public class SimpleReceiver {
-
-    private static final Logger logger = LoggerFactory.getLogger(SimpleReceiver.class);
 
     private final CountDownLatch latch = new CountDownLatch(1);
 
-    public CountDownLatch getLatch() {
-        return latch;
-    }
-
     @KafkaListener(topics = TOPIC_TEST_1, containerFactory = "simpleKafkaListenerContainerFactory")
-    public void listen1(ConsumerRecord<Integer, String> cr) {
-        logger.info(TOPIC_TEST_1 + " Received a message with key=" + cr.key() + ", value=" + cr.value());
+    public void simpleListener(ConsumerRecord<Integer, String> cr) {
+        log.info("{} Received a message with key = {} , value={}", TOPIC_TEST_1, cr.key(), cr.value());
         latch.countDown();
     }
 }
