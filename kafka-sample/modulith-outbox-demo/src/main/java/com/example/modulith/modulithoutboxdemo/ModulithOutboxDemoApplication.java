4b2269c7e0e7240c21b63ec3f9a736ad6d3f2de1
Raja Kolli <rajadilipkolli@gmail.com>
2023-12-21 08:48:52 +0000

    feat : initial commit of spring modulith

---
+++ kafka-sample/modulith-outbox-demo/src/main/java/com/example/modulith/modulithoutboxdemo/ModulithOutboxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.modulith.modulithoutboxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithOutboxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulithOutboxDemoApplication.class, args);
+	}
+
+}
---
+++ kafka-sample/modulith-outbox-demo/src/test/java/com/example/modulith/modulithoutboxdemo/ModulithOutboxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulith.modulithoutboxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithOutboxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
