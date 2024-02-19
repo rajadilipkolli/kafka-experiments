837c64a9085549371bc36f4230d30989980226b9
Raja Kolli <rajadilipkolli@gmail.com>
2024-02-19 18:34:08 +0000

    feat : fixes issue with constructor binding

--- kafka-spring/kafka-dsl-integration/src/main/java/com/example/integration/kafkadsl/KafkaDslApplication.java
+++ kafka-spring/kafka-dsl-integration/src/main/java/com/example/integration/kafkadsl/KafkaDslApplication.java
@@ -3,10 +3,10 @@ package com.example.integration.kafkadsl;
 import com.example.integration.kafkadsl.config.KafkaAppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@EnableConfigurationProperties(KafkaAppProperties.class)
+@ConfigurationPropertiesScan(basePackageClasses = KafkaAppProperties.class)
 public class KafkaDslApplication {
 
     public static void main(String[] args) {
