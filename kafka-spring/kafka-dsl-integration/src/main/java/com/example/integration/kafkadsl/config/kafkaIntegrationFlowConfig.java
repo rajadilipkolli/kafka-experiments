c3aae1ba84d52c06303eacdf90b37a27001e4f95
Raja Kolli <rajadilipkolli@gmail.com>
2024-02-28 04:09:15 +0000

    fix : letting kafka admin create the topics

--- kafka-spring/kafka-dsl-integration/src/main/java/com/example/integration/kafkadsl/config/kafkaIntegrationFlowConfig.java
+++ kafka-spring/kafka-dsl-integration/src/main/java/com/example/integration/kafkadsl/config/kafkaIntegrationFlowConfig.java
@@ -1,38 +1,30 @@
 package com.example.integration.kafkadsl.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.kafka.dsl.Kafka;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration(proxyBeanMethods = false)
 public class kafkaIntegrationFlowConfig {
 
-    private final KafkaAppProperties properties;
+    private final KafkaAppProperties kafkaAppProperties;
 
-    public kafkaIntegrationFlowConfig(KafkaAppProperties properties) {
-        this.properties = properties;
-    }
-
-    @Bean
-    KafkaAdmin.NewTopics topics() {
-        return new KafkaAdmin.NewTopics(
-                new NewTopic(properties.topic(), 32, (short) 3), new NewTopic(properties.newTopic(), 32, (short) 3));
+    public kafkaIntegrationFlowConfig(KafkaAppProperties kafkaAppProperties) {
+        this.kafkaAppProperties = kafkaAppProperties;
     }
 
     @Bean
     IntegrationFlow toKafka(KafkaTemplate<?, ?> kafkaTemplate) {
-        return flow ->
-                flow.handle(Kafka.outboundChannelAdapter(kafkaTemplate).messageKey(this.properties.messageKey()));
+        return flow -> flow.handle(
+                Kafka.outboundChannelAdapter(kafkaTemplate).messageKey(this.kafkaAppProperties.messageKey()));
     }
 
     @Bean
     IntegrationFlow fromKafkaFlow(ConsumerFactory<?, ?> consumerFactory) {
-        return IntegrationFlow.from(Kafka.messageDrivenChannelAdapter(consumerFactory, this.properties.topic()))
+        return IntegrationFlow.from(Kafka.messageDrivenChannelAdapter(consumerFactory, this.kafkaAppProperties.topic()))
                 .channel(c -> c.queue("fromKafka"))
                 .get();
     }
