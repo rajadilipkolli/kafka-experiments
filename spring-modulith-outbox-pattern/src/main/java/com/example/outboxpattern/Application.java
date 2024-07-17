57cc1b3535403b9fce04615f171271536d224450
Raja Kolli <rajadilipkolli@gmail.com>
2024-07-17 13:52:12 +0000

    revert public modifier

--- spring-modulith-outbox-pattern/src/main/java/com/example/outboxpattern/Application.java
+++ spring-modulith-outbox-pattern/src/main/java/com/example/outboxpattern/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
-class Application {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
