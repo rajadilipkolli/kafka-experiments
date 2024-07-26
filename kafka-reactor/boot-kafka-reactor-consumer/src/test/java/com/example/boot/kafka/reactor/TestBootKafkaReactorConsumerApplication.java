24a630757014cbbe76755e789d48bffa76ff233e
Raja Kolli <rajadilipkolli@gmail.com>
2024-07-26 04:50:29 +0000

    feat : using apache kafka in testcontainers

--- kafka-reactor/boot-kafka-reactor-consumer/src/test/java/com/example/boot/kafka/reactor/TestBootKafkaReactorConsumerApplication.java
+++ kafka-reactor/boot-kafka-reactor-consumer/src/test/java/com/example/boot/kafka/reactor/TestBootKafkaReactorConsumerApplication.java
@@ -10,8 +10,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
@@ -29,11 +29,10 @@ class TestBootKafkaReactorConsumerApplication {
 
     @Bean
     @ServiceConnection
-    KafkaContainer kafkaContainer(DynamicPropertyRegistry propertyRegistry) {
-        KafkaContainer kafkaContainer = new KafkaContainer(
-                        DockerImageName.parse("confluentinc/cp-kafka").withTag("7.6.2"))
-                .withKraft();
-        propertyRegistry.add("spring.kafka.bootstrapServers", kafkaContainer::getBootstrapServers);
+    KafkaContainer kafkaContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        KafkaContainer kafkaContainer =
+                new KafkaContainer(DockerImageName.parse("apache/kafka").withTag("3.7.1"));
+        dynamicPropertyRegistry.add("spring.kafka.bootstrapServers", kafkaContainer::getBootstrapServers);
         return kafkaContainer;
     }
 
