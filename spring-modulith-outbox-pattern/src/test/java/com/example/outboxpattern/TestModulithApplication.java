d79b7dc5809ac94d058410508cb36b21da041581
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-01 16:37:41 +0000

    feat : reuse postgresql container def

--- spring-modulith-outbox-pattern/src/test/java/com/example/outboxpattern/TestModulithApplication.java
+++ spring-modulith-outbox-pattern/src/test/java/com/example/outboxpattern/TestModulithApplication.java
@@ -1,6 +1,7 @@
 package com.example.outboxpattern;
 
 import com.example.outboxpattern.common.ContainersConfig;
+import com.example.outboxpattern.common.SQLContainerConfig;
 import org.springframework.boot.SpringApplication;
 
 public class TestModulithApplication {
@@ -8,7 +9,7 @@ public class TestModulithApplication {
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "local");
         SpringApplication.from(ModulithApplication::main)
-                .with(ContainersConfig.class)
+                .with(ContainersConfig.class, SQLContainerConfig.class)
                 .run(args);
     }
 }
--- spring-modulith-outbox-pattern/src/test/java/com/example/outboxpattern/common/ContainersConfig.java
+++ spring-modulith-outbox-pattern/src/test/java/com/example/outboxpattern/common/ContainersConfig.java
@@ -6,17 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfig {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("17.0-alpine"));
-    }
 
     @Bean
     @ServiceConnection
