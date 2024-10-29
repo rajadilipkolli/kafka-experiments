005b887f82df7d58404751f1acb4417b2ab92f88
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-29 22:02:11 +0000

    fix : issue with connecting to kafka

--- kafka-streams/analytics-spring-cloud-streams-kafka-consumer/src/test/java/com/example/analytics/common/ContainersConfiguration.java
+++ kafka-streams/analytics-spring-cloud-streams-kafka-consumer/src/test/java/com/example/analytics/common/ContainersConfiguration.java
@@ -4,6 +4,7 @@ package com.example.analytics.common;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -14,5 +15,12 @@ public class ContainersConfiguration {
     @ServiceConnection
     KafkaContainer kafkaContainer() {
         return new KafkaContainer(DockerImageName.parse("apache/kafka-native").withTag("3.8.1"));
+    }
+
+    @Bean
+    DynamicPropertyRegistrar kafkaProperties(KafkaContainer kafkaContainer) {
+        return (properties) -> {
+            properties.add("spring.kafka.bootstrapServers", kafkaContainer::getBootstrapServers);
+        };
     }
 }
