6c820231e2819675f581674d3de4948d94a0f42f
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-29 22:47:57 +0000

    revert changes

--- spring-modulith-outbox-pattern/src/test/java/com/example/outboxpattern/common/ContainersConfig.java
+++ spring-modulith-outbox-pattern/src/test/java/com/example/outboxpattern/common/ContainersConfig.java
@@ -3,9 +3,9 @@ package com.example.outboxpattern.common;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -13,17 +13,14 @@ public class ContainersConfig {
 
     @Bean
     @ServiceConnection
-    KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native").withTag("3.8.1"));
-    }
-
-    @Bean
-    public DynamicPropertyRegistrar kafkaProperties(KafkaContainer kafkaContainer) {
-        return (properties) -> {
-            // Connect our Spring application to our Testcontainers Kafka instance
-            properties.add("spring.kafka.consumer.bootstrap-servers", kafkaContainer::getBootstrapServers);
-            properties.add("spring.kafka.producer.bootstrap-servers", kafkaContainer::getBootstrapServers);
-        };
+    KafkaContainer kafkaContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        KafkaContainer kafkaContainer = new KafkaContainer(
+                        DockerImageName.parse("confluentinc/cp-kafka").withTag("7.7.1"))
+                .withKraft();
+        // Connect our Spring application to our Testcontainers Kafka instance
+        dynamicPropertyRegistry.add("spring.kafka.consumer.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        dynamicPropertyRegistry.add("spring.kafka.producer.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        return kafkaContainer;
     }
 
     @Bean
