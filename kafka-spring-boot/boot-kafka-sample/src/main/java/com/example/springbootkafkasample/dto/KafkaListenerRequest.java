d13d749f042c1d8c52219fdefd65a39ddc039e28
Raja Kolli <rajadilipkolli@gmail.com>
2025-01-10 15:57:01 +0000

    fix : formatting

--- kafka-spring-boot/boot-kafka-sample/src/main/java/com/example/springbootkafkasample/dto/KafkaListenerRequest.java
+++ kafka-spring-boot/boot-kafka-sample/src/main/java/com/example/springbootkafkasample/dto/KafkaListenerRequest.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record KafkaListenerRequest(
-        @NotBlank(message = "Container ID must not be blank")
-                @Pattern(regexp = "^\\S.*$", message = "Container ID must not start with whitespace")
-                String containerId,
+        @NotBlank(message = "Container ID must not be blank") @Pattern(
+                        regexp = "^[a-zA-Z0-9\\-_.]+$",
+                        message =
+                                "Container ID must contain only alphanumeric characters, hyphens, underscores, and dots")
+                @Pattern(regexp = "^\\S.*$", message = "Container ID must not start with whitespace") String containerId,
         @NotNull(message = "Operation must not be null") Operation operation) {}
