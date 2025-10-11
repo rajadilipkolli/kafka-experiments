8de29fd958e3ca4f81e9fdc7b38c2593f461f8fb
Raja Kolli <rajadilipkolli@gmail.com>
2025-10-11 13:07:15 +0000

    adds missing columns

--- spring-modulith-outbox-pattern/src/main/resources/db/changelog/migration/01-create_event_publication.sql
+++ spring-modulith-outbox-pattern/src/main/resources/db/changelog/migration/01-create_event_publication.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS event_publication
 (
-  id               UUID NOT NULL,
-  listener_id      TEXT NOT NULL,
-  event_type       TEXT NOT NULL,
-  serialized_event TEXT NOT NULL,
-  publication_date TIMESTAMP WITH TIME ZONE NOT NULL,
-  completion_date  TIMESTAMP WITH TIME ZONE,
+  id                      UUID NOT NULL,
+  listener_id             TEXT NOT NULL,
+  event_type              TEXT NOT NULL,
+  serialized_event        TEXT NOT NULL,
+  publication_date        TIMESTAMP WITH TIME ZONE NOT NULL,
+  completion_date         TIMESTAMP WITH TIME ZONE,
+  last_resubmission_date  TIMESTAMP WITH TIME ZONE,
+  completion_attempts     INTEGER NOT NULL DEFAULT 0,
+  status                  TEXT NOT NULL DEFAULT 'PENDING',
   PRIMARY KEY (id)
 );
 
