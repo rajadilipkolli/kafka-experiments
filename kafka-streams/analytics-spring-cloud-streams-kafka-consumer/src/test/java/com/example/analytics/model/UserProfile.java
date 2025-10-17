35de99e7f30376d936cceef3c85bba05458b3e30
Raja Kolli <rajadilipkolli@gmail.com>
2025-10-17 08:03:17 +0000

    fix : failures

--- kafka-streams/analytics-spring-cloud-streams-kafka-consumer/src/test/java/com/example/analytics/model/UserProfile.java
+++ kafka-streams/analytics-spring-cloud-streams-kafka-consumer/src/test/java/com/example/analytics/model/UserProfile.java
@@ -1,15 +1,75 @@
 /* Licensed under Apache-2.0 2025 */
 package com.example.analytics.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserProfile {
     private String userId;
     private String name;
     private String country;
+
+    public UserProfile() {}
+
+    public UserProfile(String userId, String name, String country) {
+        this.userId = userId;
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserProfile that = (UserProfile) o;
+
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        return country != null ? country.equals(that.country) : that.country == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{"
+                + "userId='"
+                + userId
+                + '\''
+                + ", name='"
+                + name
+                + '\''
+                + ", country='"
+                + country
+                + '\''
+                + '}';
+    }
 }
