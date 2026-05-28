aa4361d12044789a3bf7faada4d2c3536341fc00
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-28 18:26:49 +0000

    enable in other modules

--- kafka-spring-boot/boot-concurrent-kafka-consumer/src/main/java/com/example/kafka/config/KafkaConfig.java
+++ kafka-spring-boot/boot-concurrent-kafka-consumer/src/main/java/com/example/kafka/config/KafkaConfig.java
@@ -1,9 +1,5 @@
-package com.example.boot.kafka.reactor.config;
+package com.example.kafka.config;
 
-import com.example.boot.kafka.reactor.util.AppConstants;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,18 +9,10 @@ import org.springframework.kafka.core.ConsumerFactory;
 
 @EnableKafka
 @Configuration(proxyBeanMethods = false)
-class KafkaConfiguration {
-
-    private static final Logger log = LoggerFactory.getLogger(KafkaConfiguration.class);
+public class KafkaConfig {
 
     @Bean
-    NewTopic helloTopic() {
-        log.info("Creating helloTopic");
-        return new NewTopic(AppConstants.HELLO_TOPIC, 1, (short) 1);
-    }
-
-    @Bean
-    ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
+    public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
             ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
             ConsumerFactory<Object, Object> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
--- kafka-spring-boot/boot-kafka-sample/src/main/java/com/example/springbootkafkasample/config/KafkaConfig.java
+++ kafka-spring-boot/boot-kafka-sample/src/main/java/com/example/springbootkafkasample/config/KafkaConfig.java
@@ -3,11 +3,14 @@ package com.example.springbootkafkasample.config;
 import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListenerConfigurer;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistrar;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -26,6 +29,16 @@ class KafkaConfig implements KafkaListenerConfigurer {
     @Override
     public void configureKafkaListeners(@NonNull KafkaListenerEndpointRegistrar registrar) {
         registrar.setValidator(this.validator);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, consumerFactory);
+        factory.getContainerProperties().setObservationEnabled(true);
+        return factory;
     }
 
     @Bean
