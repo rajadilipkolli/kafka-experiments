dd1c53f0cd1af7f025d972a288cf4c78ba3df72c
Raja Kolli <rajadilipkolli@gmail.com>
2026-06-19 08:47:32 +0530

    fix(deps): update confluent.version to v8.3.0

--- kafka-avro/spring-boot-kafka-avro-producer/src/test/java/com/example/springbootkafkaavro/containers/KafkaContainersConfig.java
+++ kafka-avro/spring-boot-kafka-avro-producer/src/test/java/com/example/springbootkafkaavro/containers/KafkaContainersConfig.java
@@ -16,7 +16,7 @@ import org.testcontainers.utility.DockerImageName;
 public class KafkaContainersConfig {
 
     private final Network network = Network.newNetwork();
-    private static final String CONFLUENT_VERSION = "8.2.1";
+    private static final String CONFLUENT_VERSION = "8.3.0";
 
     @Bean
     @ServiceConnection
